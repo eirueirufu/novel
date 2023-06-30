df2eba7c6da9497858f7320bf4d2645e4ac9ef26
eirueirufu <731358689@qq.com>
2023-06-30 22:33:57 +0800

    feat: 侧边栏items功能按钮

--- src/extension.ts
+++ src/extension.ts
@@ -3,5 +3,21 @@ import { registerHighlight } from './heighlight'
 
 export function activate(context: vscode.ExtensionContext) {
 	registerHighlight(context);
+
+	const storageProvider = new StorageProvider()
+	vscode.window.registerTreeDataProvider('novelCharacters', storageProvider);
+	vscode.commands.registerCommand('characters.addCharacter', () => {
+		vscode.window.showInformationMessage('todo');
+	});
 }
 
+class StorageProvider implements vscode.TreeDataProvider<vscode.TreeItem> {
+	getTreeItem(element: vscode.TreeItem): vscode.TreeItem {
+		return element;
+	}
+
+	getChildren(element?: vscode.TreeItem): Thenable<vscode.TreeItem[]> {
+		const mockItems: vscode.TreeItem[] = [{ label: "人物1", contextValue: "folder" }, { label: "人物2", contextValue: "folder" }, { label: "人物3", contextValue: "folder" }];
+		return Promise.resolve(mockItems)
+	}
+}
