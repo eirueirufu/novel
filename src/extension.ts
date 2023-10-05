93b336cc73a203a1dd75b4f65354cb12ada1f012
eirueirufu <731358689@qq.com>
2023-10-05 23:08:32 +0800

    fix

--- src/extension.ts
+++ src/extension.ts
@@ -8,5 +8,6 @@ export async function activate(context: vscode.ExtensionContext) {
 	await new Completion(context).activate();
 	await new Gpt(context).activate();
 	await new Highlight(context).activate();
+	// https://github.com/antlr/antlr4/issues/4211
 	// await new Format(context).activate();
 }
