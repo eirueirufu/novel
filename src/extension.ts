07e5e01c6f984958113b2d8758844008b6c5a87b
eirueirufu <731358689@qq.com>
2023-10-05 21:27:07 +0800

    fix

--- src/extension.ts
+++ src/extension.ts
@@ -8,5 +8,5 @@ export async function activate(context: vscode.ExtensionContext) {
 	await new Completion(context).activate();
 	await new Gpt(context).activate();
 	await new Highlight(context).activate();
-	await new Format(context).activate();
+	// await new Format(context).activate();
 }
