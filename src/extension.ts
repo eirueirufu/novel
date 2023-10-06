bb4ca1d02adaccd0ecfb0cf6e033d5ca43094560
eirueirufu <731358689@qq.com>
2023-10-06 20:24:20 +0800

    fix: 修复antlr4打包问题

--- src/extension.ts
+++ src/extension.ts
@@ -8,6 +8,5 @@ export async function activate(context: vscode.ExtensionContext) {
 	await new Completion(context).activate();
 	await new Gpt(context).activate();
 	await new Highlight(context).activate();
-	// https://github.com/antlr/antlr4/issues/4211
-	// await new Format(context).activate();
+	await new Format(context).activate();
 }
